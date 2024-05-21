14d1894bfe2a62a96df393a324f439c27b583735
Hannah Liao <128202377+hannliao@users.noreply.github.com>
2024-05-21 16:27:49 -0600

    Fix menu dropdown listeners

--- src/index.js
+++ src/index.js
@@ -1,52 +1,57 @@
-import "./style.css"
+import "./style.css";
 import home from "./home.js";
 import menu from "./menu.js";
-import contact from "./contact.js"
-import githubMark from "./img/github-mark.svg"
+import contact from "./contact.js";
+import githubMark from "./img/github-mark.svg";
 
 function buttonListeners() {
-    const content = document.querySelector("#content");
-    const buttons = document.querySelectorAll("nav button");
+  const content = document.querySelector("#content");
+  const buttons = document.querySelectorAll("nav button");
 
-    buttons.forEach(button => {
-        button.addEventListener("click", () => {
-            content.textContent = "";
-            const buttonText = button.textContent;
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      content.textContent = "";
+      const buttonText = button.textContent;
 
-            switch (buttonText) {
-                case "Menu":
-                    menu();
-                    break;
-                case "Drinks":
-                    menu();
-                    const drinksSection = document.querySelector("#drinks");
-                    drinksSection.scrollIntoView({ behavior: "smooth" });
-                    break;
-                case "Ice Cream":
-                    menu();
-                    const iceCreamSection = document.querySelector("#ice-cream");
-                    iceCreamSection.scrollIntoView({ behavior: "smooth" });
-                    break;
-                case "Contact":
-                    contact();
-                    break;
-                default:
-                    home();
-                    break;
-            }
-        });
+      switch (buttonText) {
+        case "Menu":
+          menu();
+          break;
+        case "Drinks":
+          menu();
+          scrollTo("#drinks");
+          break;
+        case "Ice Cream":
+          menu();
+          scrollTo("#ice-cream");
+          break;
+        case "Contact":
+          contact();
+          break;
+        default:
+          home();
+          break;
+      }
     });
+  });
+}
 
-    const menuDiv = document.querySelector(".menu-div");
-    const dropdown = document.querySelector(".dropdown");
+function scrollTo(selector) {
+  const section = document.querySelector(selector);
+  section.scrollIntoView({ behavior: "smooth" });
+}
 
-    menuDiv.addEventListener("mouseover", () => {
-        dropdown.classList.add("visible");
-    });
+function dropdownListeners() {
+  const menuDiv = document.querySelector(".menu-div");
+  const dropdown = document.querySelector(".dropdown");
 
-    menuDiv.addEventListener("mouseout", () => {
-        dropdown.classList.remove("visible");
-    });
+  menuDiv.addEventListener("mouseover", () => {
+    dropdown.classList.add("visible");
+  });
+
+  menuDiv.addEventListener("mouseout", () => {
+    dropdown.classList.remove("visible");
+  });
 }
 
 const link = document.querySelector("footer a");
@@ -56,3 +61,4 @@ link.appendChild(github);
 
 home();
 buttonListeners();
+dropdownListeners();
